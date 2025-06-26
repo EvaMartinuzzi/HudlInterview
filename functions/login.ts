2c0a6402a8b674bc9abd729c870b1bf26255ad6a
Eva Martinuzzi <martinuzzi.eva@gmail.com>
2025-06-26 16:06:50 -0500

    create url variable

--- functions/login.ts
+++ functions/login.ts
@@ -1,8 +1,10 @@
 import { Page } from 'playwright-core';
 import { selectors } from '../selectors/login';
 
+const hudlUrl = 'https://www.hudl.com/'
+
 export async function goToLoginPage(page: Page) {
-    await page.goto(selectors.hudlUrl);
+    await page.goto(this.hudlUrl);
     await page.locator(selectors.headerLoginBtn).click();
     await page.locator(selectors.dropdownLoginBtn).click();
     await page.waitForURL(/login/);
